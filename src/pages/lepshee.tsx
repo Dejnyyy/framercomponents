624b2881bc1b77e94a02d349460cc992bc4ff506
Dejnyyy <danslaby@email.cz>
2025-01-15 23:31:21 -0500

    noo to se doresi

--- src/pages/lepshee.tsx
+++ src/pages/lepshee.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import HeroSection from "./lepsheecomponents/HeroSection";
+import SectionTwo from "./lepsheecomponents/SectionTwo";
 
 export default function Home() {
   return (
@@ -11,50 +13,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen text-white flex-col items-center justify-center bg-gradient-to-b from-black to-gray-900">
-        <motion.h1
-          className="text-5xl font-extrabold mb-12 text-center"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-        >
-          FRAMER MOTION <br /> Components
-        </motion.h1>
-        <motion.div
-          className="flex space-x-8"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 1 }}
-        >
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <Link href={"/lepsheecomponents/Carousel"}>
-              <p className="cursor-pointer text-lg font-medium px-6 py-2 rounded bg-gradient-to-tr from-blue-500 to-green-500 hover:scale-105 transform transition duration-300 shadow-lg">
-                Adjective Carousel
-              </p>
-            </Link>
-          </motion.div>
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <Link href={"/components/contactForm"}>
-              <p className="cursor-pointer text-lg font-medium px-6 py-2 rounded bg-gradient-to-tr from-yellow-500 to-orange-500 hover:scale-105 transform transition duration-300 shadow-lg">
-                 
-              </p>
-            </Link>
-          </motion.div>
-         
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <Link href={"/components/Navbar"}>
-              <p className="cursor-pointer text-lg font-medium px-6 py-2 rounded bg-gradient-to-tr from-zinc-500 to-black hover:scale-105 transform transition duration-300 shadow-lg">
-                
-              </p>
-            </Link>
-          </motion.div>
-          <motion.div whileHover={{ scale: 1.1 }}>
-            <Link href={"/lepsheecomponents/HeroSection"}>
-              <p className="cursor-pointer text-lg font-medium px-6 py-2 rounded bg-gradient-to-tr from-pink-600 to-rose-600 hover:scale-105 transform transition duration-300 shadow-lg">
-                Hero Section
-              </p>
-            </Link>
-          </motion.div>
-        </motion.div>
+       <HeroSection />
+       <div className="w-full h-64 bg-black"></div>
+       <SectionTwo />
+       
       </main>
     </>
   );
---
+++ src/pages/lepsheecomponents/SectionTwo/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Carousel from "../Carousel";
+import Image from "next/image";
+import Navbar from "../Navbar";
+
+const HeroSection = () => {
+  return (
+    <div className="relative bg-black text-white min-h-screen">
+      <div className="">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="text-center m-auto"
+      >
+        <div className="grid grid-cols-2">
+        <h1 className="text-5xl font-bold uppercase">
+          <span className="text-transparent ml-0 bg-clip-text bg-gradient-to-r to-gray-200 from-gray-500">
+            {" Stavíme kreativní "}
+          </span>
+          </h1>
+        </div>
+          <br />
+          <div className="grid grid-cols-1">
+          <h1 className="text-5xl font-bold uppercase">
+          <span className="text-transparent ml-20 bg-clip-text bg-gradient-to-r from-gray-200 to-gray-500">
+            {"digitální řešení na míru"}
+          </span>
+        </h1>
+        </div>
+        <div className="w-2/3 mx-auto">
+          <p className="mt-6 text-lg  text-zinc-400">
+          </p>
+          <p className="mt-6 text-lg  text-zinc-400 font-Montserrat">
+          Začínali jsme kreativou. Od našich začátků tvoříme krásná, ale přesto skvěle funkční řešení aplikací a všeho dalšího. Vždy jsme ovšem měli dar – dar netvořit jen pro nás, kreativce. Dar tvořit pro svět. Věděli jsme, že naše nápady se realizovat dají. Proto vznikla i technologická divize, která s naprostou precizností přetváří naše návrhy na skutečnost.
+          </p>
+        </div>
+       
+      </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
