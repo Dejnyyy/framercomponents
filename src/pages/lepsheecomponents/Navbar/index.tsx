b20933ae69088829fe47217fe50ffd7a9ffb2ae9
Dejnyyy <danslaby@email.cz>
2025-01-15 23:11:18 -0500

    tak

--- src/pages/lepsheecomponents/Navbar/index.tsx
+++ src/pages/lepsheecomponents/Navbar/index.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import LanguageSelector from "./LanguageSelector";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 1) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="fixed w-full">
       {/* Gradient Line */}
-      <div className="gradient-line"></div>
+      <motion.div
+        className={`gradient-line ${scrolled ? "opacity-100" : "opacity-0"}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: scrolled ? 1 : 0 }}
+        transition={{
+          duration: 0.5,
+        }}
+      ></motion.div>
+
+      {/* Navbar */}
       <motion.nav
         className="bg-black text-white p-4 rounded-b-3xl border-gray-700 border-b w-full flex justify-between items-center z-50"
         initial={{ y: -100 }}
